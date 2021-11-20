b735632762ba9e10c9babc9f3449304da13eb430
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-20 16:24:58 +0530

    Add uc2

---
+++ BST_UC2/BST_UC2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BST_UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.Insert(22);
+            obj1.Insert(40);
+            obj1.Insert(60);
+            obj1.Insert(95);
+            obj1.Insert(11);
+            obj1.Insert(65);
+            obj1.Insert(3);
+            obj1.Insert(16);
+            obj1.Insert(63);
+            obj1.Insert(67);
+            obj1.TraverseInorder(obj1.root);
+            Console.WriteLine();
+            obj1.TraversePostorder(obj1.root);
+            Console.WriteLine();
+            obj1.TraversePreorder(obj1.root);
+
+
+        }
+       
+    }
+}
